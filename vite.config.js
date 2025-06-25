6e02a7dd5a1b9a4df9add624e523555dfe59e099
Rohit <rohitsinghop11@example.com>
2025-06-25 09:33:10 +0530

    Add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/My_Portfolio-main/', // Replace with your repo name
   plugins: [react()],
-  build: {
-    outDir: 'dist' // This is optional, it's default for Vite
-  }
-});
+})
